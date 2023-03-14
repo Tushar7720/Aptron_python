936592ab03c78bf3e4d8ffa4ee0399a5718f0af1
Tushar7720 <tushar007720@gmail.com>
2023-03-14 05:08:58 -0700

    These files have basics of python and strings in python

---
+++ basics.py
@@ -0,0 +1,29 @@
+# programmer defined input to add two numbers, use of Print Statement.
+
+a = 5
+b = 6
+c = a + b;
+print(c)
+
+# input function and its cases
+
+d = input("Enter d: ")  # default class --> str
+e = input("Enter e: ")
+
+f = d + e
+
+print(f)
+
+#type casting
+
+g = int(d) + int(e)
+print(g)
+
+# split function to Casscade input
+
+h,i = input("enter h and i : ").split()  # by default usses space as separator
+j = int(h) + int(i)
+
+print(j)
+
+
---
+++ stringfunctions.py
@@ -0,0 +1,34 @@
+#WAP to  display string functions
+
+name = "tushar agarwal"
+
+# indexing
+
+print(name[0])
+print(name[-14])
+
+#slicing
+
+print(name[0:8])
+print(name[:])
+
+#Methods
+# 1. Length
+
+print(len(name))
+
+# 2. lower
+
+print(name.lower())
+
+# 3 . upper
+
+print( name.upper())
+
+# 4. count
+
+print(name.count('a'))
+
+# 5. tittle - convers string into tittle case
+
+print(name.title())
