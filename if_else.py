6b1745dbd1793b2d5568cea6123e973ba023f8ed
Tushar7720 <tushar007720@gmail.com>
2023-03-15 11:33:34 -0700

    This Program is to demonstrate if elif else

---
+++ if_else.py
@@ -0,0 +1,12 @@
+# wap to assign ticket charge based on age
+
+a = int(input(" pleae enter you age :"))
+
+if a>=0 and a<=5 :
+    print("the ticket is free")
+
+elif a>5 and a<14 :
+    print("The ticket will be half priced")
+
+else:
+    print("Sorry the Ticket will be regularly priced")
