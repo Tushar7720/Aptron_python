02eb5968fa4accc7bc57d49181fb41ca1265f656
Tushar7720 <tushar007720@gmail.com>
2023-03-17 05:43:42 -0700

    to check whether a number is postive/negative

---
+++ positive_negative.py
@@ -0,0 +1,9 @@
+# Wap to check whether a number is positive or negative
+
+n = int(input("Enter the number that you want to check :"))
+
+if n >= 0:
+    print(n," is positive")
+
+else:
+    print(n," is negative")
