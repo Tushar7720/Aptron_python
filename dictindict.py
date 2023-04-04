180da775ef97fea7b5222ba630350312358a7496
Tushar7720 <tushar007720@gmail.com>
2023-04-03 22:44:10 -0700

    Demonstrates dictionary in dictionary

---
+++ dictindict.py
@@ -0,0 +1,22 @@
+#Demonstrate dictionary in dictionary
+
+Student_logs = {
+    "St_1":{
+        "name":"Chetan",
+        "age": 19,
+        "Rank": 32
+    },
+    "St_2": {
+        "name": "Raman",
+        "age": 20,
+        "Rank": 11
+    },
+    "St_3": {
+        "name": "aniket",
+        "age": 19,
+        "Rank": 1
+    }
+
+}
+
+print(Student_logs)
