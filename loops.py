ba6f9d7bc892de73cb67f6d51fd7aa76a2c73fc4
Tushar7720 <tushar007720@gmail.com>
2023-06-10 23:49:30 -0700

    loops

---
+++ loops.py
@@ -0,0 +1,11 @@
+#wap for for loop
+
+l1 = [10,12,23,34,56,67,78,75,23,43]
+l2 = []
+for i in l1:
+    if (i < 50):
+        l2.append((i))
+
+print(l2)
+print(dir(l1))
+# dir print the attributes or functions of the object
